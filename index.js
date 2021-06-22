55e91381f335daacd6b57cbb5607ca72e05763d2
Andrew-Michaelis <amichaelis70@s.tooeletech.edu>
2021-06-22 14:23:42 -0600

    Cleaned and Commented
    testing and extra code has been removed, comments added, and everything generally cleaned up a bit. Ready for submission, though not entirely as feature complete as I would have liked. Missing are an upper limit on the flex box and a button to erase all entries in the database.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const app = express();
-const PORT = 3000;
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
-const TodoTask = require("./models/TodoTasks");
-require("dotenv").config({path: ".env" });
-var back = "#bada55";
-var text = "#000000";
+const express = require("express"); //I need it
+const app = express(); //It's important
+const PORT = 3000; //Not actually important, but makes it easy to change the port and have it apply throughout the code
+const dotenv = require("dotenv"); //I use it
+const mongoose = require("mongoose"); //Really Really useful
+const TodoTask = require("./models/TodoTasks"); //What makes the above actually useful
+require("dotenv").config({path: ".env" }); //I need to connect to my database
+var back = "#bada55"; //Need to declare before I send the page to the client or else crash
+var text = "#000000"; //Same as above
 
 //hide DB connection
 dotenv.config()
@@ -15,90 +15,58 @@ mongoose.connect(process.env.DATABASE_CONNECT, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
 },
-console.log("connected")
+// console.log("connected") //Useful tool in debug
 )
 //view engine configuration
-app.set("view engine", "ejs");
-app.use("/static", express.static("public"));
+app.set("view engine", "ejs"); //I'm using embedded javascript
+app.use("/static", express.static("public")); //something something, I don't actually know
 app.use(express.urlencoded({extended: true }));
 //Get method
-app.get("/", (req, res) => {
-    TodoTask.find({}, (err, tasks) => {
-        res.render("todo.ejs", {todoTasks: tasks, back:back, text:text});
+app.get("/", (req, res) => { //when root path is loaded, get these
+    TodoTask.find({}, (err, tasks) => { //check the database
+        res.render("todo.ejs", {todoTasks: tasks, back:back, text:text}); //render the database and my color options
     });
 });
 //POST method
-app.post("/", async (req, res) => {
-    const todoTask = new TodoTask({
-        name: req.body.name,
-        bgColor: req.body.bgColor,
-        textColor: req.body.textColor,
+app.post("/", async (req, res) => { //when root gives me something
+    const todoTask = new TodoTask({ //create a todoTask based on my model
+        name: req.body.name, //it contains this
+        bgColor: req.body.bgColor, //and this
+        textColor: req.body.textColor, //and also this
     });
-    try {
-        await todoTask.save();
-        res.redirect("/");
-    } catch (err) {
-        console.log(err);
-        res.redirect("/");
+    try { //once I have things
+        await todoTask.save(); //try adding it to the database
+        res.redirect("/"); //return to root
+    } catch (err) { //if it fails
+        console.log(err); //tell me why
+        res.redirect("/"); //and return to root
     }
 });
-//Update method
-// app
-//     .route("/edit/:id")
-//     .get((req,res)=>{
-//         const id = req.params.id;
-//         TodoTask.find({}, (err,tasks)=>{
-//             res.render("todoEdit.ejs", {todoTasks: tasks, idTask: id });
-//         });
-//     })
-//     .post((req,res)=>{
-//         const id = req.params.id;
-//         TodoTask.findByIdAndUpdate(id, { name: req.body.name }, (err) => {
-//             if(err) return res.send(500,err);
-//             res.redirect("/");
-//         });
-//     });
 //Delete method
 app
-    .route("/remove/:id")
-    .get((req,res)=>{
-        const id = req.params.id;
-        TodoTask.findByIdAndRemove(id,(err)=>{
-            if(err) return res.send(500,err);
-            res.redirect("/");
+    .route("/remove/:id") //if I press a thing that has the remove route (by id)
+    .get((req,res)=>{ //I want to get some info
+        const id = req.params.id; //what was the id?
+        TodoTask.findByIdAndRemove(id,(err)=>{ //find that id and remove it from the database
+            if(err) return res.send(500,err); //if you screw that up, tell me why
+            res.redirect("/"); //return to root
         });
     });
 // Apply Changes to Page
 app
-    .route("/select/:id")
-    .get((req,res)=>{
-        const id = req.params.id;
-        TodoTask.findById(id, (err, docs)=>{
-            if(err) return console.log(err);
-            else {
-                back = docs.bgColor;
-                text = docs.textColor;
-                console.log("bg: "+back+" text: "+text);
-                res.redirect("/");
-                // return {back, text}
+    .route("/select/:id") //if I press a thing that has the select route (by id)
+    .get((req,res)=>{ //I want to get some info
+        const id = req.params.id; //what was the id?
+        TodoTask.findById(id, (err, docs)=>{ //find that id in the database
+            if(err) return console.log(err); //if you screw that up, tell me why
+            else { //otherwise
+                back = docs.bgColor; //I'm changing a variable used in rendering my root path
+                text = docs.textColor; //this one too
+                res.redirect("/"); //return to root (re-render, theres new data here this time)
             }
         });
     })
-function senders(){
-    return new Promise(colorful =>{
-        colorful = [back,text];
-    });
-}
-// app
-//     .route("/select/:id")
-//     .post("/", async (req,res)=>{
-//         try{
-//             senders();
-//         }catch(err){
-//             console.log(err);
-//         }
-//     })
 //Server Start
-app.listen(PORT, () => {
-    console.log(`Server is up and running on port: ${PORT}`);
+app.listen(PORT, () => { //Pop open a server on port PORT(3000)
+    console.log(`Server is up and running on port: ${PORT}`); //tell me what port I'm using
 });
--- models/TodoTasks.js
+++ models/TodoTasks.js
@@ -1,8 +1,8 @@
-const mongoose = require("mongoose");
-const todoTaskSchema = new mongoose.Schema({
+const mongoose = require("mongoose"); //this is a mongoose thing, of course I need mongoose
+const todoTaskSchema = new mongoose.Schema({ //I'm building a template to use later
     name: {
         type: String,
-        required: true,
+        required: true, //the name is REQUIRED, the other things not so much
     },
     bgColor: {
         type: String,
@@ -12,8 +12,8 @@ const todoTaskSchema = new mongoose.Schema({
     },
     date: {
         type: Date,
-        default: Date.now,
+        default: Date.now, //I don't actually give this data anywhere, it adds itself in
     },
 });
 
-module.exports = mongoose.model("TodoTask", todoTaskSchema);
+module.exports = mongoose.model("TodoTask", todoTaskSchema); //this template is used elsewhere, of course I make sure other places can use it
